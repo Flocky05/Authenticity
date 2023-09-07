d41246d629b9d55821f07a0c4c0718c22c5e7db5
Flocky05 <md.rakibul.islam.0501@gmail.com>
2023-09-07 09:43:53 +0600

    added register section

--- src/component/Register/Register.jsx
+++ src/component/Register/Register.jsx
@@ -1,9 +1,14 @@
-import React from 'react';
+
 import './Register.css'
 const Register = () => {
     return (
         <div>
-            <h2>This is register page</h2>
+            <h2>Please register Now</h2>
+            <form>
+                <input type="email" name='email'id='email' placeholder='enter your email' /> <br />
+                <input type="password" name='password' id='password' placeholder='enter your password' /><br />
+                <input type="submit" value="register" />
+            </form>
         </div>
     );
 };
