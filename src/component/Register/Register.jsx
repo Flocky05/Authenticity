b24a9155791866dc71020d0ef896cec2fe27d69b
Flocky05 <md.rakibul.islam.0501@gmail.com>
2023-09-07 10:01:48 +0600

    update header section

--- src/component/Register/Register.jsx
+++ src/component/Register/Register.jsx
@@ -1,10 +1,14 @@
 
 import './Register.css'
+const handleSubmit =(event)=>{
+    console.log(event.target);
+}
 const Register = () => {
     return (
+        
         <div>
             <h2>Please register Now</h2>
-            <form>
+            <form onSubmit={handleSubmit} >
                 <input type="email" name='email'id='email' placeholder='enter your email' /> <br />
                 <input type="password" name='password' id='password' placeholder='enter your password' /><br />
                 <input type="submit" value="register" />
