a9a8ad42a804775e3e6b97d6745f7506808d470d
Flocky05 <md.rakibul.islam.0501@gmail.com>
2023-09-07 09:07:03 +0600

    added router dom and header

---
+++ src/component/About Us/AboutUS.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './AboutUs.css'
+const AboutUS = () => {
+    return (
+        <div>
+            <h2>WE are very great</h2>
+        </div>
+    );
+};
+
+export default AboutUS;
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,43 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Home from './component/Home/Home.jsx';
+import Login from './component/Login/Login.jsx';
+import Register from './component/Register/Register.jsx';
+import Main from './component/Layout/Main.jsx';
+import AboutUS from './component/About Us/AboutUS.jsx';
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Main></Main>,
+    children:[
+     {
+      path: "/",
+      element: <Home></Home>,
+     }
+     , {
+        path:"/login",
+        element: <Login></Login>,
+      },
+      {
+        path:"register",
+        element: <Register></Register>,
+      },
+      {
+        path:"about-us",
+        element:<AboutUS></AboutUS>,
+      },
+    ]
+  },
+  
+]);
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+     <RouterProvider router={router} />
   </React.StrictMode>,
 )
