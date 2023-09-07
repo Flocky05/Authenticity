9867d632eb6528d88d0adad6042aa4e2e4dbe06f
Flocky05 <md.rakibul.islam.0501@gmail.com>
2023-09-07 11:39:26 +0600

    update email and password

--- src/component/Register/Register.jsx
+++ src/component/Register/Register.jsx
@@ -2,18 +2,20 @@
 import './Register.css'
 const handleSubmit =(event)=>{
     event.preventDefault()
-    console.log(event.target.email.value);
+    const email=event.target.email.value;
+    const password=event.target.password.value;
+    console.log(email,password);
 }
 const Register = () => {
     return (
         
         <div className='register-section'>
-            <div>
-            <h2>Please register Now</h2>
-            <form onSubmit={handleSubmit} >
-                <input type="email" name='email'id='email' placeholder='enter your email' /> <br />
-                <input type="password" name='password' id='password' placeholder='enter your password' /><br />
-                <input type="submit" value="register" />
+            <div className='text-center'>
+            <h2 className='text-2xl font-medium'>Please register Now</h2>
+            <form onSubmit={handleSubmit}>
+                <input  className='bg-white p-2 m-2 border-2 rounded-md' type="email" name='email'id='email' placeholder='enter your email' /> <br />
+                <input className='bg-white p-2 m-2 border-2 rounded-md' type="password" name='password' id='password' placeholder='enter your password' /><br />
+                <input className='bg-green-500 rounded-lg text-white px-4 py-2 hover:bg-green-700' type="submit" value="register" />
             </form>
             </div>
         </div>
