a1bef373bcdbb6fa6df89dfb6d7be655409cccde
Flocky05 <md.rakibul.islam.0501@gmail.com>
2023-09-08 12:13:06 +0600

    added favicons and authentication

--- src/component/Register/Register.jsx
+++ src/component/Register/Register.jsx
@@ -1,21 +1,36 @@
-
+import { getAuth ,createUserWithEmailAndPassword } from "firebase/auth";
+import app from "../../firebase/firebase.config";
 import './Register.css'
-const handleSubmit =(event)=>{
-    event.preventDefault()
-    const email=event.target.email.value;
-    const password=event.target.password.value;
-    console.log(email,password);
-}
+
+const auth = getAuth(app);
 const Register = () => {
+    const handleSubmit =(event)=>{
+        event.preventDefault()
+        const email=event.target.email.value;
+        const password=event.target.password.value;
+        console.log(email,password);
+
+    createUserWithEmailAndPassword(auth, email, password)
+  .then((result) => {
+    // Signed in 
+    const SignedUser = result.user;
+    console.log(SignedUser);
+    // ...
+  })
+  .catch((error) => {
+   console.error(error);
+    // ..
+  });
+    }
     return (
         
-        <div className='register-section'>
+        <div className='register-section m-20'>
             <div className='text-center'>
-            <h2 className='text-2xl font-medium'>Please register Now</h2>
+            <h2 className='text-2xl font-medium'>Please Register Now</h2>
             <form onSubmit={handleSubmit}>
-                <input  className='bg-white p-2 m-2 border-2 rounded-md' type="email" name='email'id='email' placeholder='enter your email' /> <br />
-                <input className='bg-white p-2 m-2 border-2 rounded-md' type="password" name='password' id='password' placeholder='enter your password' /><br />
-                <input className='bg-green-500 rounded-lg text-white px-4 py-2 hover:bg-green-700' type="submit" value="register" />
+                <input  className='bg-white p-2 m-2 border-2 rounded-md w-96' type="email" name='email'id='email' placeholder='enter your email' /> <br />
+                <input className='bg-white p-2 m-2 border-2 rounded-md w-96' type="password" name='password' id='password' placeholder='enter your password' /><br />
+                <input className='bg-green-500 rounded-lg text-white px-6 py-2 hover:bg-green-700' type="submit" value="register" />
             </form>
             </div>
         </div>
---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCXOSaNZA0u-BpTkGu9AvZkJLGeZ_B6jOQ",
+  authDomain: "authenticity-786bb.firebaseapp.com",
+  projectId: "authenticity-786bb",
+  storageBucket: "authenticity-786bb.appspot.com",
+  messagingSenderId: "532187689109",
+  appId: "1:532187689109:web:30c429af3d6438eb0c2b9c",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
