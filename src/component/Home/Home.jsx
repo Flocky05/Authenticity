d18a9a03f41b7b6f4006291cdcf924ae94649734
Flocky05 <md.rakibul.islam.0501@gmail.com>
2023-09-09 12:48:16 +0600

    added home page

--- src/component/Home/Home.jsx
+++ src/component/Home/Home.jsx
@@ -1,9 +1,15 @@
-
+import { Link } from "react-router-dom";
+import img from "../../assets/images/home/home.avif"
 import './Home.css'
 const Home = () => {
     return (
-        <div>
-            <h2 className='bg-red-100 text-center text-4xl p-4 h-40 text-white'>This is Home page</h2>
+        <div className="relative">
+            <img className="w-full h-screen" src={img} alt="" />
+            <div className="text-white absolute -mt-80 text-center">
+                <h3 className="text-4xl font-bold p-4 ">Authenticity</h3>
+                <p className="px-20">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nam eveniet animi non a? Consequatur aliquam voluptatem quo nulla necessitatibus officiis voluptatum praesentium sint. Dignissimos distinctio unde ipsam fuga ratione asperiores!</p>
+                <Link className="bg-blue-400 px-3 py-1 rounded text-white">Learn more</Link>
+            </div>
         </div>
     );
 };
