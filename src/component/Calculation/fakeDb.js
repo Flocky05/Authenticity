1031b62dc529834f92b1598a57b31144521b68a8
Flocky05 <md.rakibul.islam.0501@gmail.com>
2023-09-10 23:57:48 +0600

    added functionality of localStorage

--- src/component/Calculation/fakeDb.js
+++ src/component/Calculation/fakeDb.js
@@ -1,5 +1,11 @@
 /* a local Storage manage  */
 const addToDb = (id) => {
-  localStorage.setItem(id, 1);
+  const quantity = localStorage.getItem(id);
+  if (quantity) {
+    const newQuantity = parseInt(quantity) + 1;
+    localStorage.setItem(id, newQuantity);
+  } else {
+    localStorage.setItem(id, 1);
+  }
 };
 export { addToDb };
