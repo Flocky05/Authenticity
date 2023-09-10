2dc87db3a0204cb3cad7473642d2259bd1fe3abd
Flocky05 <md.rakibul.islam.0501@gmail.com>
2023-09-10 11:16:26 +0600

    update cosmetic section

--- src/component/Cosmetic/Cosmetic.jsx
+++ src/component/Cosmetic/Cosmetic.jsx
@@ -1,9 +1,10 @@
 const Cosmetic = (props) => {
-  const { name, price } = props.data;
+  const { name, price,_id } = props.data;
   return (
-    <div>
-      <h3>Cosmetic name:{name}</h3>
-      <p>Cosmetic price:{price}</p>
+    <div className="bg-blue-500 border-2 rounded-xl p-10 m-12 text-white">
+      <h3 className="text-3xl font-semibold">Cosmetic name:<span className="text-red-500">{name}</span></h3>
+      <p className="text-xl ">Cosmetic price: {price}</p>
+      <p className="text-lg ">Cosmetic id is:{_id}</p>
     </div>
   );
 };
