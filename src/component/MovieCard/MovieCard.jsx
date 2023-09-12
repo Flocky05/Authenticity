c8f8fb1e7d6c827224d7d10e9658a948dbfc8d8c
Flocky05 <md.rakibul.islam.0501@gmail.com>
2023-09-12 14:11:03 +0600

    update movie card

--- src/component/MovieCard/MovieCard.jsx
+++ src/component/MovieCard/MovieCard.jsx
@@ -4,11 +4,14 @@ const MovieCard = (props) => {
     const {Title,Released,Plot,Language,Poster}=props.data;
     return (
         <div className="bg-gray-800 text-white w-11/12 my-5 mx-auto rounded-xl">
-            <img className="w-full" src={Poster} alt="" />
-            <h2>Movie Name: {Title}</h2>
-            <p>Released Date :{Released} </p>
-            <p>plot :{Plot} </p>
-            <p>Language :{Language} </p>
+            <img className="w-full opacity-80 hover:opacity-100" src={Poster} alt="" />
+            <div className="p-4">
+                <h2>Movie Name: <span className="text-xl font-bold hover:text-orange-600">{Title}</span> </h2>
+                <p>Released Date :{Released} </p>
+                <p>plot :{Plot} </p>
+                <p className="mb-4">Language :{Language} </p>
+                <a className="bg-blue-800 px-4 py-1 rounded-lg" href="/movieDetails">more</a>
+            </div>
         </div>
     );
 };
