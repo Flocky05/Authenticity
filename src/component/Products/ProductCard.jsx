c5966236736c77c3b0711dc2989e916fd1e7f241
Flocky05 <md.rakibul.islam.0501@gmail.com>
2023-09-14 21:26:04 +0600

    update product card and products json

--- src/component/Products/ProductCard.jsx
+++ src/component/Products/ProductCard.jsx
@@ -3,7 +3,7 @@ import AddToCard from "../../assets/images/add to card/add-to-card.png"
 const ProductCard = (props) => {
     const {name,img,price,stock,ratings,seller}=props.data;
     return (
-        <div className="border-2 m-8 rounded-md">
+        <div className="border-2 m-8 rounded-md ">
             <img className="p-1" src={img} alt="" />
             <div className="pl-2 mb-0">
                 <p className="text-lg font-semibold">{name} </p>
@@ -14,7 +14,7 @@ const ProductCard = (props) => {
                     <p>Ratting:{ratings}</p>
                 </div>
             </div>
-            <div className="flex justify-center items-center bg-orange-200 -mb-0">
+            <div className="flex justify-center items-center bg-orange-200">
                     <p>Add to Cart</p>
                     <img className=" h-8" src={AddToCard} alt="" />
             </div>
