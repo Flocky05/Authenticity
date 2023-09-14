c9b3a6f76b707973a962342206fa5f77987c788e
Flocky05 <md.rakibul.islam.0501@gmail.com>
2023-09-14 13:22:21 +0600

    update product section

--- src/component/Products/ProductCard.jsx
+++ src/component/Products/ProductCard.jsx
@@ -1,14 +1,23 @@
 
-
+import AddToCard from "../../assets/images/add to card/add-to-card.png"
 const ProductCard = (props) => {
-    const {name,img,price,stock,category}=props.data;
+    const {name,img,price,stock,ratings,seller}=props.data;
     return (
-        <div>
-            <h2>{name} </h2>
-            <img src={img} alt="" />
-            <p>{price} </p>
-            <p>{stock} </p>
-            <p>{category} </p>
+        <div className="border-2 m-8 rounded-md">
+            <img className="p-1" src={img} alt="" />
+            <div className="pl-2 mb-0">
+                <p className="text-lg font-semibold">{name} </p>
+                <p>price:${price} </p>
+                <p>stock: {stock} </p>
+                <div className="py-3 text-sm">
+                    <p>seller:{seller}</p>
+                    <p>Ratting:{ratings}</p>
+                </div>
+            </div>
+            <div className="flex justify-center items-center bg-orange-200 -mb-0">
+                    <p>Add to Cart</p>
+                    <img className=" h-8" src={AddToCard} alt="" />
+            </div>
         </div>
     );
 };
