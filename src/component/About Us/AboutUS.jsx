6de1f48283723869a340472facc03ead4a4673b9
Flocky05 <md.rakibul.islam.0501@gmail.com>
2023-09-15 09:25:14 +0600

    update cosmetic section

--- src/component/About Us/AboutUS.jsx
+++ src/component/About Us/AboutUS.jsx
@@ -1,24 +1,35 @@
 
 import { useEffect, useState } from 'react';
 import Cosmetic from '../Cosmetic/Cosmetic.jsx';
-import './AboutUs.css'
+
 const AboutUS = () => {
     const [cosmetics,setCosmetic]=useState([]);
-
+    const [products,setProduct]=useState([]);
     useEffect(()=>{
         fetch('data.json')
         .then(res=>res.json())
         .then(data=>setCosmetic(data))
-    },[])
+    },[]);
+    const handleAddToCart=(product)=>{
+        const newProduct=[...products,product];
+        setProduct(newProduct);
+    }
     return (
-        <div className='text-center'>
-            <h2 className='text-3xl font-bold'>Welcome to are shope</h2>
-            {
-                cosmetics.map(cosmetic=><Cosmetic 
-                    key={cosmetic.id}
-                    data={cosmetic}
-                    ></Cosmetic>)
-            }
+        <div className='flex justify-between text-center'>
+            <div>
+                <h2 className='text-3xl font-bold'>Welcome to are shope</h2>
+                {
+                    cosmetics.map(cosmetic=><Cosmetic 
+                        key={cosmetic.id}
+                        data={cosmetic}
+                        handleAddToCart={handleAddToCart}
+                        ></Cosmetic>)
+                }
+            </div>
+            <div className='bg-blue-200 w-1/4'>
+                <h2 className='text-xl font-semibold'>The Calculation section </h2>
+                <h3 className='text-start p-4'>Total clicked for Buy :{products.length} </h3>
+            </div>
         </div>
     );
 };
--- src/component/Cosmetic/Cosmetic.jsx
+++ src/component/Cosmetic/Cosmetic.jsx
@@ -1,18 +1,16 @@
-import { addToDb } from "../Calculation/fakeDb";
+
 
 const Cosmetic = (props) => {
   const { name, price,id,img } = props.data;
-  const addToCart=(id)=>{
-    addToDb(id);
-  }
-  const addToCardWithParam=()=>addToCart(id);
+  const handleAddToCart =props.handleAddToCart;
+  
   return (
     <div className="bg-blue-950 border-2 rounded-xl my-10 text-white w-2/4 mx-auto drop-shadow-xl">
       <img className="w-full rounded-lg" src={img} alt="" />
       <h3 className="text-3xl font-semibold">Cosmetic name:<span className="text-red-500">{name}</span></h3>
       <p className="text-xl ">Cosmetic price:$ {price}</p>
       <p className="text-lg ">Cosmetic id is:{id}</p>
-      <button onClick={addToCardWithParam} className="bg-green-800 px-4 py-1 rounded-lg my-4">Add to Cart</button>
+      <button onClick={()=>{handleAddToCart(props.data)}}  className="bg-green-800 px-4 py-1 rounded-lg my-4">Add to Cart</button>
     </div>
   );
 };
